75b9f5fce44ab4db63bd8b78a81e1d41654c509b
zjyz2024 <zjyz2024@proton.me>
2025-04-27 17:18:59 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -24,8 +24,8 @@ async fn setup_environment() {
         ("NEZHA_SERVER", "nz.abc.com"),
         ("NEZHA_PORT", "5555"),
         ("NEZHA_KEY", ""),
-        ("ARGO_DOMAIN", ""),  // argo固定隧道也可在scrects中添加环境变量
-        ("ARGO_AUTH", ""),    // argo密钥，留空将使用临时隧道
+        //("ARGO_DOMAIN", ""),  // argo固定隧道也可在scrects中添加环境变量
+        //("ARGO_AUTH", ""),    // argo密钥，留空将使用临时隧道
         ("CFIP", "www.visa.com.tw"),
         ("CFPORT", "443"),
         ("NAME", "shuttle"),
